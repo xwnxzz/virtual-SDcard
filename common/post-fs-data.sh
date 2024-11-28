c598e5f7519914f7f23d68330d9a75f06b48bba6
xwnxzz <xiaozhongze4328@gmail.com>
2024-11-29 04:33:01 +0800

    Create post-fs-data.sh

---
+++ common/post-fs-data.sh
@@ -0,0 +1,20 @@
+#!/system/bin/sh
+MODDIR=${0%/*}
+
+# 确保挂载点存在
+mkdir -p /mnt/sdcard
+
+# 挂载块设备
+mount -o rw /dev/block/by-name/rannki /mnt/sdcard
+
+# 修改 storage.xml 文件以识别为 SD 卡
+if [ -f /data/system/storage.xml ]; then
+    # 备份原文件
+    cp /data/system/storage.xml /data/system/storage.xml.bak
+
+    # 插入 SD 卡配置
+    sed -i '/<volumes>/a\    <volume mountPoint="/mnt/sdcard" fsType="vfat" state="mounted" removable="true" emulated="false" owner="0" />' /data/system/storage.xml
+
+    # 确保权限正确
+    chmod 600 /data/system/storage.xml
+fi
